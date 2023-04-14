722083d0b3aa164b25e1c665ff13c2711e5383e6
Arnau478 <arnauxabia@gmail.com>
2023-04-14 14:53:22 +0200

    test.sh now runs Xephyr on a 1280x720 pixel screen

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-Xephyr :1 & # Open xephyr in parallel
+Xephyr :1 -screen 1280x720 & # Open xephyr in parallel
 while [ ! -f /tmp/.X1-lock ]; do :; done # Wait for the X lock
 env DISPLAY=:1 zig-out/bin/luminance & # Start luminance
 wait # Wait for parallel processes to finish
